a45ca604628e9411bbf18e0e430e636044908e46
xukailun <xukailun@umich.edu>
2020-01-18 14:43:26 -0500

    xukailun

--- Assets/Scripts/RunJumping.cs
+++ Assets/Scripts/RunJumping.cs
@@ -6,16 +6,19 @@ public class RunJumping : MonoBehaviour
 {
     Rigidbody rigid;
     public float Jumppower = 10;
-    private float Movespeed = 3;
+    private float Movespeed = 4;
     private float currentSpeed;
+    private float duration;
+    private float durationthereshold = 0.15f;
     void Awake()
     {
-
+        duration = 0;
         rigid = this.GetComponentInParent<Rigidbody>();
     }
     // Update is called once per frame
     void Update()
     {
+        
         Vector3 newVelocity = rigid.velocity;
         if (Input.GetAxis("Horizontal") == 0) {
             newVelocity.x = currentSpeed;
@@ -24,8 +27,13 @@ public class RunJumping : MonoBehaviour
         }
         currentSpeed = newVelocity.x;
         if (Input.GetKey(KeyCode.Z) && PlayerState.instance.longpress == 0) {
-            PlayerState.instance.longpress = 1;
-            newVelocity.y = Jumppower;
+            duration += Time.deltaTime;
+            if (duration > durationthereshold) {
+                newVelocity.y = Jumppower;
+                PlayerState.instance.longpress = 1;
+            }
+        } else {
+            duration = 0;
         }
         
         rigid.velocity = newVelocity;
