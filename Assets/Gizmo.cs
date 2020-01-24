4101af10c03509bc2420b740027c0cafd8f72898
xukailun <xukailun@umich.edu>
2020-01-23 23:31:46 -0500

    skree implemented

---
+++ Assets/Gizmo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gizmo : MonoBehaviour
+{
+    private void OnDrawGizmos() {
+        Collider col = this.GetComponent<Collider>();
+        float radius = col.bounds.extents.x - 0.05f;
+        float fullDistance = 0.05f;
+        Gizmos.DrawSphere(col.bounds.center - new Vector3(0, fullDistance, 0), radius);
+    }
+}
