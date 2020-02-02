72922884214421c9337730f96349b395ba6a0ce0
Ayassaka <jack.zwx@icloud.com>
2020-02-02 11:38:29 -0500

    extra scene modified / camera follower reset

--- Assets/Scripts/DoorsNRooms/CameraFollower.cs
+++ Assets/Scripts/DoorsNRooms/CameraFollower.cs
@@ -6,46 +6,27 @@ public class CameraFollower : MonoBehaviour
 {
     public RoomInfo room;
     public float freeZoneSizeX = 1.2f;
-    public float freeZoneSizeY;
-
-    public float moveHorizontalMax = 20f;
-    private bool cameraMoving = false;
-    private float tempy;
+    public float freeZoneSizeY = 1.8f;
     // Update is called once per frame
-    public float time;
     void Update()
     {
         Vector3 cam_pos = transform.position;
         Vector3 player_pos = PlayerState.instance.transform.position;
-        if (Input.GetKey(KeyCode.DownArrow) && PlayerState.instance.isGrounded()) {
-            cameraMoving = true;
-            if (time < 2) {
-                time += Time.deltaTime;
-            }
-            cam_pos.y = tempy - moveHorizontalMax * time;
-        }
-        // if (!room.isCameraBoundedHorizontally(player_pos.x, freeZoneSizeX)) {
-        if (!cameraMoving){
-            tempy = cam_pos.y;
+        
+        if (!room.isCameraBoundedHorizontally(player_pos.x, freeZoneSizeX)) {
             if (player_pos.x - cam_pos.x < -freeZoneSizeX) {
                 cam_pos.x = player_pos.x + freeZoneSizeX;
             } else if (player_pos.x - cam_pos.x > freeZoneSizeX) {
                 cam_pos.x = player_pos.x - freeZoneSizeX;
             }
-        // }
-        // if (!room.isCameraBoundedVertically(player_pos.y, freeZoneSizeY)) {
+        }
+        if (!room.isCameraBoundedVertically(player_pos.y, freeZoneSizeY)) {
             if (player_pos.y - cam_pos.y < -freeZoneSizeY) {
                 cam_pos.y = player_pos.y + freeZoneSizeY;
             } else if (player_pos.y - cam_pos.y > freeZoneSizeY) {
                 cam_pos.y = player_pos.y - freeZoneSizeY;
             }
         }
-        // }
-        if (Input.GetKeyUp(KeyCode.DownArrow)) {
-            StartCoroutine(cameraMoveBack(time));
-        }
-        // cam_pos.x = PlayerState.instance.transform.position.x;
-        // cam_pos.y = PlayerState.instance.transform.position.y;
         transform.position = cam_pos;
     }
 
@@ -64,14 +45,5 @@ public class CameraFollower : MonoBehaviour
             yield return null;
         }
         transform.position = to;
-    }
-    IEnumerator cameraMoveBack(float t) {
-        float currtime = t;
-        time = 0;
-        for (int i = 0; i < 10; ++i) {
-            transform.position += new Vector3(0, moveHorizontalMax * t / 10, 0);
-            yield return new WaitForSeconds(time / 10);
-        }
-        cameraMoving = false;
     }
 }
